6570aaabf33340c21275a638b3f92375b0bd77ef
Fauzan-F <fauzanfirdaus1966@gmail.com>
2025-11-17 15:50:54 +0700

    commit

--- src/components/customer-insights/types.ts
+++
@@ -1,7 +0,0 @@
-export interface CustomerInsightsType {
-  period: string
-  totalCustomers: number
-  newCustomers: number
-  returningCustomers: number
-  vipCustomers: number
-}
--- src/components/skeleton.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Skeleton({ className = '', style = {} }: { className?: string; style?: React.CSSProperties }) {
-  return <div className={`skeleton ${className}`} style={style} />;
-}
